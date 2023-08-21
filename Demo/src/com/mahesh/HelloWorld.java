eb40fef3834c9e1b4abd0ab64c7f64ba1c7a5c8c
2484898 <2484898@01hw2169077>
2023-08-21 13:56:05 +0530

    First Commit

--- Demo/src/com/mahesh/HelloWorld.java
+++ Demo/src/com/mahesh/HelloWorld.java
@@ -3,8 +3,7 @@ package com.mahesh;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("this is first Git project");
+		System.out.println("Test Project Git");
 
 	}
 
